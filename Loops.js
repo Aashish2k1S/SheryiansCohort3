7dd0b7f95a5117eacdebb2289e8939a20139342e
Aashish2k1S <aashishshaw123kolkata@gmail.com>
2026-04-14 02:01:26 +0530

    WHILE LOOPS PROBLEM PART 1

--- Loops.js
+++ Loops.js
@@ -244,3 +244,54 @@ while (true) {
     x++;
     if (x > 3) break;
 }
+
+
+
+// # WHILE LOOPS PROBLEM PART 1 
+
+// SUM OF DIGITS OF A NUMBER 
+function sumOfDigits(n) {
+    // Write your logic here
+    if (n <= 1 || n % 10 === 0) return n; 
+
+    let sum = 0, remain = n, temp = 0; 
+    while(true){
+        temp = remain % 10; 
+        remain = Math.floor(remain / 10); 
+        sum += temp; 
+
+        if (remain === 0) break; 
+    }
+    return sum; 
+}
+
+module.exports = { sumOfDigits };
+
+
+//CHECK IF A NUMBER IS AUTOMORPHIC 
+
+function isAutomorphic(n) {
+    // Write your logic here
+    if (n<=0) return 'No'; 
+    
+    let remain = n, temp = 0, sq = n*n, temp2 = 0; 
+    let isAutomorphic = true; 
+
+    while(true){
+        temp = remain % 10; 
+        remain = Math.floor(remain / 10); 
+        temp2 = sq % 10; 
+        sq = Math.floor(sq / 10); 
+
+        
+        if (temp !== temp2) {
+            isAutomorphic = false; 
+            break; 
+        }
+
+        if (remain === 0) break; 
+    }
+    return (isAutomorphic ? 'Yes' : 'No'); 
+}
+
+module.exports = { isAutomorphic };
--- test.js
+++ test.js
@@ -1,31 +1,29 @@
-function fact(n){
-  if (n === 0 || n === 1) return 1; 
-  else if (n < 0) return undefined; 
-  else {
-    let fact = 1; 
-    for (let i = 2; i <= n; i++) fact *= i; 
-    return fact; 
-  }
-}
-
-function isStrongNumber(n) {
-  if (n < 0) return "No";
-
-  let sum = 0, remain = n, temp = 0; 
-  while (true){
-    temp = remain % 10; 
-    remain = Math.floor(remain/ 10);     
+function isAutomorphic(n) {
+    // Write your logic here
+    if (n<=0) return 'No'; 
     
-    sum += fact(temp); 
+    let remain = n, temp = 0, sq = n*n, temp2 = 0; 
+    let isAutomorphic = true; 
 
-    if (remain === 0) break;       
-  }
+    while(true){
+        temp = remain % 10; 
+        remain = Math.floor(remain / 10); 
+        temp2 = sq % 10; 
+        sq = Math.floor(sq / 10); 
 
-  return (sum === n ? 'Yes' : 'No');
+        
+        if (temp !== temp2) {
+            isAutomorphic = false; 
+            break; 
+        }
+
+        if (remain === 0) break; 
+    }
+    return (isAutomorphic ? 'Yes' : 'No'); 
 }
 
 
-console.log(isStrongNumber(123));
+console.log(isAutomorphic(7));
 
 
 

fabe1c62ab0feb8d77fc3f7418d87877925bc694
Aashish <aashishshaw123kolkata@gmail.com>
2026-05-26 00:08:53 +0530

    array part 2

--- JS DSA/test.js
+++ JS DSA/test.js
@@ -1,7 +1,12 @@
-// Copy and Reverse Array
-function copyAndReverseArray(arr) {
-    // Write your logic here
-    return arr.slice().reverse();
-}
+class Solution {
 
-console.log(copyAndReverseArray([1,2,3,4,5]));
+    moveZeros(arr) {
+        // write your code here
+        for (let i = 0; i < arr.length; i++) {
+            arr.push(arr[arr.indexOf(0)]);
+            arr.splice(arr.indexOf(0), 1);
+        }
+        return arr;
+    }
+}
+console.log(new Solution().moveZeros([0, 1, 0, 3, 12])); // Output: [1, 3, 12, 0, 0]

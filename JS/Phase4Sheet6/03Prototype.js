e117980393efb3569e9d2d9d061705e3d56aa63b
Aashish <aashishshaw123kolkata@gmail.com>
2026-06-09 00:55:10 +0530

    JS sheet solving

--- JS/Phase4Sheet6/03Prototype.js
+++ JS/Phase4Sheet6/03Prototype.js
@@ -1,9 +1,9 @@
 // ## Problem 10: Prototype Lookup
 // Create:
 // ```jsx
-const person = {
-    name: "Rahul"
-};
+// const person = {
+//     name: "Rahul"
+// };
 // ```
 // Check whether:
 // ```jsx
@@ -36,11 +36,11 @@ const person = {
 // 10
 // ```
 
-function sum() {
-    return this.reduce((a,b) => a+b);
-}
+// function sum() {
+//     return this.reduce((a, b) => a + b);
+// }
 
-let arr = [1,2,3,4]; 
+// let arr = [1, 2, 3, 4];
 
 // console.log(Array.prototype);
 // Array.prototype.sum = sum;
@@ -48,7 +48,7 @@ let arr = [1,2,3,4];
 
 
 // console.log(arr.__proto__);
-// // arr.prototype.sum = sum; //X
+// arr.prototype.sum = sum; //X
 // arr.__proto__.sum = sum; 
 // console.log(arr.__proto__);
 
@@ -102,11 +102,11 @@ let arr = [1,2,3,4];
 // }
 // let dog = Object.create(animal);
 
-// // console.log(animal.prototype);
+// console.log(animal.prototype);
 // console.log(animal.__proto__);
 // console.log(Object.prototype);
-// // console.log(dog);
-// // dog.eat();
+// console.log(dog);
+// dog.eat();
 
 
 
@@ -128,4 +128,110 @@ let arr = [1,2,3,4];
 // start()
 // stop()
 // ```
+
+// let vehicle = {
+//     start: function() { console.log(`Starting ${this.brand} ${this.model}...`) },
+//     stop: function() { console.log(`Stopping ${this.brand} ${this.model}...`) }
+// };
+
+// let car = {
+//     brand: "Maruti",
+//     model: "Swift"
+// };
+// let bike = {
+//     brand: "Honda",
+//     model: "CBR"
+// };
+// let truck = {
+//     brand: "Tata",
+//     model: "Ace"
+// };
+
+// this will create a new object and assign it to car.prototype, which will break the prototype chain. So we should not do this. 
+// car.prototype = vehicle;
+
+// Instead, we should set the prototype of car to vehicle. This way, car will inherit from vehicle and we can access the start and stop methods from car, bike and truck.
+// car.__proto__ = vehicle;
+// bike.__proto__ = vehicle;
+// truck.__proto__ = vehicle;
+
+// console.log(car.__proto__);
+// console.log(bike.__proto__);
+// console.log(truck.__proto__);
+// console.log(Object.prototype);
+// console.log(car);
+// console.log(car.__proto__);
+
+// this will work because car.__proto__ is vehicle, which has the start method. So it will look for the start method in car, then in car.__proto__ (which is vehicle) and find it there.
+// car.start();
+// bike.stop();
+// truck.start();
+
+
+
+// ## Problem 14: Constructor Function + Prototype
+// Create a constructor:
+// ```jsx
+// Person
+// ```
+// that accepts:
+// ```jsx
+// name
+// age
+// ```
+// Add a method using:
+// ```jsx
+// Person.prototype.greet
+// ```
+// ### Expected Output
+// ```
+// Hi, I am Rahul
+// ```
+
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+
+//this is just with the anonymous function 
+// Person.prototype.greet = function() {
+//     console.log(`Hi, I am ${this.name}`);
+// };
+
+//this is with the named function, which is better for debugging and readability.
+// function greet () {
+//     console.log(`Hi, I am ${this.name}`);
+// };
+
+// Person.prototype.greet = greet;
+
+// let p1 = new Person("Rahul", 25);
+// console.log(p1);
+// p1.greet();
+
+
+
+// ## Problem 15: Prototype Chain Investigation
+// Create:
+// ```jsx
+// const arr = [];
+// ```
+// Print:
+// ```jsx
+// arr.__proto__
+// arr.__proto__.__proto__
+// arr.__proto__.__proto__.__proto__
+// ```
+// Explain the output.
+
+// const arr = [];
+// console.log(arr.__proto__); //type of Array so its prototype is Array.prototype
+// console.log(arr.__proto__.__proto__); //type of Object so its prototype is Object.prototype
+// console.log(arr.__proto__.__proto__.__proto__); //null because Object.prototype is the end of the prototype chain and its prototype is null. 
+
+//Hence, Array is also an Object because it inherits from Object.prototype. This is why we can use Object methods on arrays, such as push, pop, etc. but we can't use Array methods on objects, such as length, etc. because they don't inherit from Array.prototype.
+
+
+
 
---
+++ JS/Phase4Sheet6/04ES6.js
@@ -0,0 +1,208 @@
+// ## Problem 16: Basic Class
+// Create a class:
+// ```jsx
+// Student
+// ```
+// with:
+// ```jsx
+// name
+// course
+// ```
+// Add a method:
+// ```jsx
+// introduce()
+// ```
+// ### Expected Output
+// ```
+// I am Anubhav and I study MERN Stack
+// ```
+
+// class Student {
+//     constructor(name, course) {
+//         this.name = name;
+//         this.course = course;
+//     }
+// };
+
+// Student.__proto__.introduce = function() {
+//     console.log(`I am ${this.name} and I study ${this.course}`);
+// };
+
+// console.log(Student.__proto__.__proto__);
+// student1.__proto__.introduce();
+
+
+// Student.prototype.introduce = function() {
+//     console.log(`I am ${this.name} and I study ${this.course}`);
+// };
+
+// const student1 = new Student("Anubhav", "MERN Stack");
+
+// console.log(student1.__proto__);
+
+// student1.introduce();
+
+
+
+
+// ## Problem 17: Employee Management
+// Create a class:
+// ```jsx
+// Employee
+// ```
+// with:
+// ```jsx
+// name
+// salary
+// ```
+// Add methods:
+// ```jsx
+// increaseSalary()
+// showSalary()
+// ```
+
+// class Employee {
+//     constructor(name, salary) {
+//         this.name = name;
+//         this.salary = salary;
+//     }
+// };
+// function increaseSalary() {
+//     this.salary += 1000;
+// };
+// function showSalary() {
+//     console.log(`My salary is ${this.salary}`);
+// };
+
+// console.log(Employee.prototype);
+
+// Employee.prototype.increaseSalary = increaseSalary;
+// Employee.prototype.showSalary = showSalary;
+
+// console.log(Employee.prototype);
+
+// const employee1 = new Employee("Anubhav", 50000);
+// employee1.__proto__.showSalary = showSalary;
+// employee1.__proto__.increaseSalary = increaseSalary;
+// console.log(employee1.__proto__);
+
+// employee1.showSalary();
+// employee1.increaseSalary();
+// employee1.showSalary();
+
+
+
+
+// ## Problem 18: Bank Account System
+// Create a class:
+// ```jsx
+// BankAccount
+// ```
+// Features:
+// - Deposit
+// - Withdraw
+// - Check Balance
+// ### Bonus
+// Prevent withdrawing more than available balance.
+
+// class BankAccount {
+//     constructor(owner, balance) {
+//         this.owner = owner;
+//         this.balance = balance;
+//     }
+// };
+
+// function deposit(amount) {
+//     this.balance += amount;
+// };
+// function withdraw(amount) {
+//     if (amount > this.balance) {
+//         console.log("Insufficient balance");
+//     } else {
+//         this.balance -= amount;
+//     }
+// };
+// function checkBalance() {
+//     console.log(`Your balance is ${this.balance}`);
+// };
+
+// console.log(BankAccount.prototype);
+// BankAccount.prototype.methods = {deposit, withdraw, checkBalance};
+// console.log(BankAccount.prototype);
+// BankAccount.prototype.deposit = deposit;
+// BankAccount.prototype.withdraw = withdraw;
+// BankAccount.prototype.checkBalance = checkBalance;  
+// console.log(BankAccount.prototype);
+
+// const account1 = new BankAccount("Anubhav", 10000);
+// account1.checkBalance();
+// account1.deposit(5000);
+// account1.checkBalance();
+// account1.withdraw(2000);
+// account1.checkBalance();
+// account1.withdraw(15000);
+// account1.checkBalance();
+
+// account1.withdraw(15000);
+// account1.withdraw(10000);
+// account1.checkBalance();
+
+
+
+// ## Problem 19: Inheritance Challenge
+// Create:
+// ```jsx
+// Animal
+// ```
+// class.
+// Create:
+// ```jsx
+// Dog
+// ```
+// class using:
+// ```jsx
+// extends
+// ```
+// Methods:
+// ```jsx
+// eat()
+// bark()
+// ```
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+};
+
+
+class Dog extends Animal {
+    constructor(name) {
+        super(name);
+
+        function eat_test() {
+            console.log(`${this.name} is eating`);
+        };
+        function bark_test() {
+            console.log(`${this.name} is barking`);
+        }
+    };
+
+    eat() {
+        console.log(`${this.name} is eating`);
+    }
+    bark() {
+        console.log(`${this.name} is barking`);
+    }
+};
+
+// console.log(Dog.prototype);
+
+const dog1 = new Dog("Buddy");
+dog1.eat();
+dog1.bark();
+
+console.log(dog1.__proto__);
+
+dog1.eat_test();
+dog1.bark_test();

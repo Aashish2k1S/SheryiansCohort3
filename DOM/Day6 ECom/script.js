62ba4a7a1a3b20289980a57cb9b0d639aaa8e102
Office Aashish <aashisshaw123kolkata@gmail.com>
2026-06-24 19:01:37 +0530

    comlpeted DOM Day5 E-Comm

--- DOM/Day6 ECom/script.js
+++ DOM/Day6 ECom/script.js
@@ -1,167 +1,201 @@
+import { task } from "./tasks.js";
+
+//#region elements
+
 const body = document.body;
-const create = document.querySelector('#create');
-const theme = document.querySelector('#theme');
+const create = document.querySelector("#create");
+const theme = document.querySelector("#theme");
 
-const hero = document.querySelector('#hero');
+const main = document.querySelector("main");
 
-const formDiv = document.querySelector('#form');
-const form = document.querySelector('form');
-const close = document.querySelector('#close');
+const hero = document.querySelector("#hero");
 
+const formDiv = document.querySelector("#form");
+const form = document.querySelector("form");
+const imgUrlInp = document.querySelector("#imgUrlInp");
 
+const close = document.querySelector("#close");
+
+//#endregion elements
+
+localStorage.removeItem("products");
 
 const themeToggler = () => {
-    if (body.classList.toggle('dark')) {
-        theme.innerHTML = '<i class="ri-moon-line"></i>';
-    } else {
-        theme.innerHTML = '<i class="ri-sun-line"></i>';
-    }
-}
+    theme.innerHTML = body.classList.toggle("dark")
+        ? '<i class="ri-sun-line"></i>'
+        : '<i class="ri-moon-line"></i>';
+};
 
-function product(name, price, desc, image) {
+function Product(id, name, price, desc, image) {
+    this.id = parseInt(id);
     this.name = name;
-    this.price = price;
+    this.price = parseFloat(price).toFixed(2);
     this.desc = desc;
-    this.image = image
+    this.image = image;
 }
-let products = JSON.parse(localStorage.getItem('products')) || [
-    {
-        name: 'asdasd', 
-        price: '132123', 
-        desc: 'sadfafsadasfasfsdgsfsf',
-        image: 'https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8' 
-    },{
-        name: 'asdasd', 
-        price: '132123', 
-        desc: 'sadfafsadasfasfsdgsfsf',
-        image: 'https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8' 
-    },{
-        name: 'asdasd', 
-        price: '132123', 
-        desc: 'sadfafsadasfasfsdgsfsf',
-        image: 'https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8' 
-    },{
-        name: 'asdasd', 
-        price: '132123', 
-        desc: 'sadfafsadasfasfsdgsfsf',
-        image: 'https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8' 
-    },{
-        name: 'asdasd', 
-        price: '132123', 
-        desc: 'sadfafsadasfasfsdgsfsf',
-        image: 'https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8' 
-    },{
-        name: 'asdasd', 
-        price: '132123', 
-        desc: 'sadfafsadasfasfsdgsfsf',
-        image: 'https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8' 
-    },{
-        name: 'asdasd', 
-        price: '132123', 
-        desc: 'sadfafsadasfasfsdgsfsf',
-        image: 'https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8' 
-    }
-];
+let products = JSON.parse(localStorage.getItem("products")) || task;
 
+const card = (id, name, price, desc, image) => {
+    name =
+        name.trim().length > 15
+            ? name.trim().slice(0, 15) + "..."
+            : name.trim();
+    desc =
+        desc.trim().length > 15
+            ? desc.trim().slice(0, 15) + "..."
+            : desc.trim();
+    price = parseFloat(price).toFixed(2);
 
-const card = (index, name, price, desc, image) => {
     return `
-        <div class="card">
-            <img src="${image}" alt="img${index}" class="img${index}">
-            <div class="mid">
-                <div class="content">
-                    <h3 class="name">${name}</h3>
-                    <i class="ri-shopping-bag-line cart" onclick="cart(${index})"></i>
-                </div>
-                <p class="price">${price}</p>
-                <p class="desc">${desc}</p>
+        <div class="card"> 
+            <div class="prodImg" data-id="${id}"> 
+                <img src="${image}" alt="${name}" class="img${id}"/> 
+            </div> 
+            <div class="mid" data-id="${id}"> 
+                <h1 class="id">${id}</h1> 
+                <h2 class="name">${name}</h2> 
+                <h4 class="price">$ ${price}</h4> 
+                <small class="desc">${desc}</small> 
+            </div> 
+            <div class="lower"> 
+                <i class="ri-delete-bin-6-line delete" data-id="${id}"></i>
+                <i class="ri-shopping-bag-line cart" data-id="${id}"></i>
             </div>
-            <div class="lower">
-                <i class="ri-edit-box-line update" onclick="edit(${index})"></i>
-                <i class="ri-delete-bin-6-line delete" onclick="del(${index})"></i>
-            </div>
-
         </div>
     `;
 };
 
 const renderProductCard = () => {
-    hero.innerHTML = '';
-    if (products.length > 0) {
-        console.log(products);
-
-        products.forEach(({ name, price, desc, image }, index) => {
-            hero.innerHTML += card(index, name, price, desc, image);
-        })
-    }
-    else {
-        hero.innerHTML = `<div id="noProduct">no products yet!!</div>`;
-    }
-
-}
-const edit = (index) => {
-    formDiv.style.display = "flex";
-    let {name, price, desc, img} = products[index]; 
-    form.name.value = name;
-    form.price.value = price;
-    form.desc.value = desc;
-    form.img.value = img;
-}
-    
-
-const del = (index) => {
-    let consent = confirm('Are sure you want to delete this product?'); 
-    if (!consent) return; 
-
-    products.splice(index, 1);
-
-    products.length > 0 
-    ? localStorage.setItem('products', JSON.stringify(products)) 
-    : localStorage.removeItem('products'); 
-
-    renderProductCard(); 
+    hero.innerHTML = "";
+    hero.innerHTML =
+        products.length > 0
+            ? products
+                  .map(({ id, name, price, desc, image }) => {
+                      return card(id, name, price, desc, image);
+                  })
+                  .join("")
+            : `<div id="noProduct">no products yet!!</div>`;
 };
 
+const formVisibility = (mode) => {
+    form.reset();
 
+    if (mode === 1) {
+        formDiv.style.display = "flex";
+        main.classList.add("main-overflow");
+    } else {
+        formDiv.style.display = "none";
+        main.classList.remove("main-overflow");
+    }
 
-theme.addEventListener('click', themeToggler);
+    sampleFormImg.src = "";
+    sampleFormImg.style.display = "none";
+};
+
+const edit = (prodId) => {
+    let index = products.findIndex((p) => p.id === prodId);
+    formVisibility(1);
+
+    let { id, name, price, desc, image: img } = products[index];
+
+    form[0].value = id;
+    form[1].value = name;
+    form[2].value = price;
+    form[3].value = desc;
+    form[4].value = img;
+};
+
+const del = (id) => {
+    let consent = confirm(`Are sure you want to delete the product-${id}?`);
+    if (!consent) return;
+
+    let index = products.findIndex((p) => p.id === id);
+    products.splice(index, 1);
+
+    products.length > 0
+        ? localStorage.setItem("products", JSON.stringify(products))
+        : localStorage.removeItem("products");
+
+    renderProductCard();
+};
+
+const submit = (e) => {
+    e.preventDefault();
+
+    const id = parseInt(
+        e.target[0].value || Math.max(...products.map(({ id }) => id)) + 1,
+    );
+    const name = e.target[1].value.trim();
+    const price = parseFloat(e.target[2].value).toFixed(2);
+    const desc = e.target[3].value.trim();
+    const img = e.target[4].value.trim();
+
+    if (!id) {
+        alert("Product ID is Invalid!!!");
+        return;
+    }
+
+    console.log(img);
+
+    if (!name || !price || !price || !desc || !img) {
+        alert("Please fillout the manadate fields!!!");
+        return;
+    }
+
+    products.unshift(new Product(id, name, price, desc, img));
+    localStorage.setItem("products", JSON.stringify(products));
+    renderProductCard();
+    formVisibility(0);
+};
 
 const pageLoad = () => {
     themeToggler();
     renderProductCard();
-}
+};
 
 pageLoad();
 
+theme.addEventListener("click", themeToggler);
+create.addEventListener("click", () => formVisibility(1));
+close.addEventListener("click", () => formVisibility(0));
+form.addEventListener("submit", (e) => submit(e));
+imgUrlInp.addEventListener("input", () => {
+    let testElem = document.querySelectorAll(".sample");
+    testElem.forEach((e) => e.remove());
 
-create.addEventListener('click', () => {
-    formDiv.style.display = "flex";
+    let src = imgUrlInp.value.trim();
+    if (!src) return;
+
+    const sampleFormImg = document.createElement("img");
+
+    sampleFormImg.setAttribute("id", "sampleFormImg");
+    sampleFormImg.setAttribute("alt", "sample");
+    sampleFormImg.classList.add("sample");
+
+    try {
+        sampleFormImg.src = src;
+        imgUrlInp.after(sampleFormImg);
+    } catch (e) {
+        sampleFormImg.remove();
+        alert("Invalid URL!!");
+    }
 });
-close.addEventListener('click', () => {
-    form.reset();
-    formDiv.style.display = "none";
-});
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const name = e.target[0].value;
-    const price = e.target[1].value;
-    const desc = e.target[2].value;
-    const img = e.target[3].value;
-
-    if (
-        name.trim() === "" || price.trim() === "" ||
-        parseFloat(price.trim()) <= 0 ||
-        desc.trim() === "" || img.trim() === ""
-    )
-        alert('Please fillout the manadate fields!!!');
-
-    products.push(new product(name, price, desc, img));
-    localStorage.setItem('products', JSON.stringify(products));
-    renderProductCard();
-    form.reset();
-});
-
-
-
-
+hero.addEventListener(
+    "click",
+    (e) => {
+        if (e.target.closest(".prodImg")) {
+            let id = parseInt(e.target.closest(".prodImg").dataset.id);
+            edit(id);
+        } else if (e.target.closest(".mid")) {
+            let id = parseInt(e.target.closest(".mid").dataset.id);
+            edit(id);
+        } else if (e.target.closest(".delete")) {
+            let id = parseInt(e.target.closest(".delete").dataset.id);
+            del(id);
+        } else if (e.target.closest(".cart")) {
+            let id = parseInt(e.target.closest(".cart").dataset.id);
+            alert("clicked on cart for id " + id);
+        }
+    },
+    true,
+);
---
+++ DOM/Day6 ECom/tasks.js
@@ -0,0 +1,44 @@
+export const task = [
+    {
+        id: 1,
+        name: "asdasd",
+        price: "132123",
+        desc: "sadfafsadasfasfsdgsfsf",
+        image: "https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+    },
+    {
+        id: 2,
+        name: "asdasd",
+        price: "132123",
+        desc: "sadfafsadasfasfsdgsfsf",
+        image: "https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+    },
+    {
+        id: 3,
+        name: "asdasd",
+        price: "132123",
+        desc: "sadfafsadasfasfsdgsfsf",
+        image: "https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+    },
+    {
+        id: 4,
+        name: "asdasd",
+        price: "132123",
+        desc: "sadfafsadasfasfsdgsfsf",
+        image: "https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+    },
+    {
+        id: 5,
+        name: "asdasd",
+        price: "132123",
+        desc: "sadfafsadasfasfsdgsfsf",
+        image: "https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+    },
+    {
+        id: 6,
+        name: "asdasd",
+        price: "132123",
+        desc: "sadfafsadasfasfsdgsfsf",
+        image: "https://plus.unsplash.com/premium_photo-1780691020752-213011fadb66?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+    },
+];

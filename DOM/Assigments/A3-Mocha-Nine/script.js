66a80058c23a33fb7e0719d1c970b2538cbbc31a
Office Aashish <aashisshaw123kolkata@gmail.com>
2026-07-06 18:06:15 +0530

    working on timer ui

---
+++ DOM/Assigments/A3-Mocha-Nine/script.js
@@ -0,0 +1,16 @@
+// #region SELECTORS
+
+const body = document.body;
+
+
+
+const dashboard = document.querySelector('#dashboard')
+const sideBar = document.querySelector('#sideBar')
+
+const todo = document.querySelector('#todo')
+const plan = document.querySelector('#plan')
+const quotes = document.querySelector('#quotes')
+const timer = document.querySelector('#timer')
+const goals = document.querySelector('#goals')
+
+// #endregion SELECTORS
